dcf00fdcd07cc30f934af4b50dc88e349b98be4b
Collin Cromwell <collcrom@gmail.com>
2017-09-26 15:39:52 -0500

    fixed the morph function

--- script/app.js
+++ script/app.js
@@ -1,15 +1,21 @@
 imageArray = ['images/imageedit_12_4456809058.png','images/imageedit_17_7862663924.png','images/imageedit_23_3911380816.png'];
-
+imageCounter = 0;
 class tomagotchi {
 	constructor(name, image){
+		imageCounter++
 		this.name = name;
 		this.age = 1;
 		this.hunger = 1;
 		this.sleepiness = 1;
 		this.boredom = 1;
 		this.image = image;
+		$('.flex-container').append(`<img src=${this.image}>`);
 		const temp = setInterval( () => {
-			this.age += .5;
+			this.age += 1;
+			if(!(this.age%5)){
+				console.log('poop');
+				this.morph(imageArray[imageCounter++])
+			}
 			this.hunger++;
 			this.boredom++;
 			this.sleepiness++;
@@ -27,7 +33,9 @@ class tomagotchi {
 	}
 
 	morph(newImage){
+		console.log('poop')
 		this.image = newImage;
+		$('img').attr('src',this.image);
 	}
 }
 
@@ -36,6 +44,8 @@ $('#feed-me-button').on('click', () => {
 		jimbo.hunger--;
 		if(jimbo.hunger > 10)
 			jimbo.hunger = 10;
+		if(jimbo.hunger < 1)
+			jimbo.hunger = 1;
 		$('#feed-me-data').text(`Hunger: ${jimbo.hunger}`);	
 	}
 });
@@ -45,6 +55,8 @@ $('#playtime-button').on('click', () => {
 		jimbo.boredom--;
 		if(jimbo.boredom > 10)
 			jimbo.boredom = 10;
+		if(jimbo.boredom < 1)
+			jimbo.boredom = 1;
 		$('#playtime-data').text(`Boredom: ${jimbo.boredom}`);
 	}
 });
@@ -55,7 +67,9 @@ $('#bedtime-button').on('click', (event) => {
 		jimbo.sleepiness--;
 		if(jimbo.sleepiness > 10)
 			jimbo.sleepiness = 10;
+		if(jimbo.sleepiness < 1)
+			jimbo.sleepiness = 1;
 		$('#bedtime-data').text(`Sleepiness: ${jimbo.sleepiness}`);
 	}
 })
-const jimbo = new tomagotchi('jim', imageArray[0]);
+const jimbo = new tomagotchi('jim', imageArray[imageCounter]);
